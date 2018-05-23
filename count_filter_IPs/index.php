f834aee1a8fb67ac3ffb291e5bd4a5adb55cfae0
zhengxiaoyu <petezh@yeah.net>
2018-05-23 10:02:52 +0800

    修复bug：改为post传值。优化代码。

--- count_filter_IPs/index.php
+++ count_filter_IPs/index.php
@@ -1,9 +1,18 @@
 <?php
 ini_set('max_execution_time', '120');
-$min = 0;
-$show_num = 0;
-if (!empty($_GET['min'])) $min = $_GET['min'];
-if (isset($_GET['show_num'])) $show_num = $_GET['show_num'];
+
+$flag = false;//是否开始工作
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $flag = true;
+    $min = 0;
+    $show_num = 0;
+    if (!empty($_POST['min'])) {
+        $min = $_POST['min'];
+    }
+    if (isset($_POST['show_num'])) {
+        $show_num = $_POST['show_num'];
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +22,7 @@ if (isset($_GET['show_num'])) $show_num = $_GET['show_num'];
     <title>统计IP个数</title>
 </head>
 <body>
-<form action="" method="get">
+<form action="" method="post">
     <dl>
         <dt>是否显示IP地址出现次数</dt>
         <dd>
@@ -30,72 +39,78 @@ if (isset($_GET['show_num'])) $show_num = $_GET['show_num'];
 <?php
 
 // 获取当前系统时间，返回float格式，单位：秒
-function get_time() {
+function get_time()
+{
     date_default_timezone_set('Asia/Shanghai');
     list($usec, $sec) = explode(" ", microtime());
     return ((float)$usec + (float)$sec);
 }
 
-$filter = [];
-$res = fopen('./filter.txt', 'r');
-while (!feof($res)) {
-    $line = fgets($res);
-    $line = str_replace("\r\n", "", $line);
-    $filter[] = $line;
-}
-$filter0 = $filter;
-##############################
-$IPs = [];
-$res = fopen('./www_access.log', 'r');
-$line_num = 1;
-$begin = get_time();
-$content = file_get_contents('./www_access.log');
-/**/
-while (!feof($res)) {
-    $line = fgets($res);
-    $index1 = strpos($line, '[') + 1;
-    $index2 = strpos($line, ']');
-    $target = substr($line, $index1, $index2 - $index1);
+if ($flag) {
+    #读取filter.txt
+    $begin = get_time();#开始计时
+    $filter = [];
+    $res = fopen('./filter.txt', 'r');
+    while (!feof($res)) {
+        $line = fgets($res);
+        $line = str_replace("\r\n", "", $line);
+        $filter[] = $line;
+    }
+    $filter0 = $filter;
+    echo "读取filter.txt耗时：" . (get_time() - $begin) . " (s)<br/>";
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    $begin = get_time();#开始计时
+    $IPs = [];
+    $res = fopen('./www_access.log', 'r');
+    $content = file_get_contents('./www_access.log');
 
-    if ($target) {//IP地址不为空
-        if (!in_array($target, $filter0)) {//不存在于$filter0
-            if (array_key_exists($target, $IPs)) {//存在于$IPs
-                $IPs[$target]++;
-            } else {//不存在于$IPs
-                $IPs[$target] = 1;
+    while (!feof($res)) {
+        $line = fgets($res);
+        $index1 = strpos($line, '[') + 1;
+        $index2 = strpos($line, ']');
+        $ip = substr($line, $index1, $index2 - $index1);
+
+        if ($ip) {//IP地址不为空
+            if (!in_array($ip, $filter0)) {//不存在于$filter0
+                if (array_key_exists($ip, $IPs)) {//存在于$IPs
+                    $IPs[$ip]++;
+                } else {//不存在于$IPs
+                    $IPs[$ip] = 1;
+                }
+            }
+
+            if (!in_array($ip, $filter)) {//不存在于$filter（尚未加入filter.txt）
+                if ($IPs[$ip] >= $min) {//符合条件，加入filter.txt
+                    $filter[] = $ip;//加入$filter
+                    file_put_contents('./filter.txt', $ip . "\r\n", FILE_APPEND);
+                }
             }
         }
 
-        if (!in_array($target, $filter)) {//不存在于$filter
-            if ($IPs[$target] >= $min) {//次数不小于最小次数，加入filter.txt
-                $filter[] = $target;//加入$filter
-                file_put_contents('./filter.txt', $target . "\r\n", FILE_APPEND);
+    }
+
+    echo "读取www_access.txt耗时：" . (get_time() - $begin) . " (s)<br/>";
+    $begin = get_time();#开始计时
+
+    fclose($res);
+    arsort($IPs);
+    $count = 0;
+    foreach ($IPs as $ip => $num) {
+        if ($show_num) {//显示次数
+            if ($num >= $min) {
+                echo "$ip --> $num<br/>";
+                $count++;
+            }
+        } else {//不显示次数
+            if ($num >= $min) {
+                echo "$ip<br/>";
+                $count++;
             }
         }
     }
-
+    echo "输出结果耗时：" . (get_time() - $begin) . " (s)<br/>";
+    echo '符合要求的IP地址个数：', $count, '<br/>';
 }
-
-echo "耗时：".(get_time() - $begin) . " s<br/>";
-
-fclose($res);
-arsort($IPs);
-$count = 0;
-foreach ($IPs as $ip => $num) {
-    if ($show_num) {//显示次数
-        if ($num >= $min) {
-            echo "$ip --> $num<br/>";
-            $count++;
-        }
-    } else {//不显示次数
-        if ($num >= $min) {
-            echo "$ip<br/>";
-            $count++;
-        }
-    }
-}
-echo '总数：', $count, '<br/>';
-
 ?>
 </body>
 </html>
