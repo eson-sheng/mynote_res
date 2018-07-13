df83af4edaee9295354240e50948c3284273f694
zhengxiaoyu <petezh@yeah.net>
2018-07-13 15:17:32 +0800

    路径配置放到config.php，并且可以覆盖

--- log_analysis/common.php
+++ log_analysis/common.php
@@ -1,16 +1,16 @@
 <?php
-$log_paths = [
-    './logs.txt',
-    './logs2.txt'
-];
+include_once 'config.php';
 include_once 'pdo.php';
 include_once "../config.php";
 
 $log_path = $log_paths[$sign - 1];
 $indexfile_path = "./last_end_index$sign.txt";
+if (!file_exists($indexfile_path)) {
+    file_put_contents($indexfile_path, 0);
+}
 $last_end_index = file_get_contents($indexfile_path);
 $content = file_get_contents($log_path, false, null, $last_end_index);
-if($content===''){
+if ($content === '') {
     die("日志 $log_path 没有更新内容。");
 }
 $last_end_index += strlen($content);
