167c4864f511e999488e8ec8e756a321595af2c6
yuebin.chen <cyb@piaojuke.com>
2018-09-02 11:33:25 +0800

    从其他项目更新common.php

--- common.php
+++ common.php
@@ -1,5 +1,6 @@
 <?php
 
+global $ct_path, $ct_log_path;
 $log_path = "test_php.txt";
 // 是否先log到buffer，再通过CT_flush()一次性写入文件
 $ct_log_buffer = true;
@@ -7,11 +8,22 @@ $CT_off = true;
 $request_num = str_pad(rand(0, 0x7fffffff), 10, "0", STR_PAD_LEFT);
 $CT_format = "";
 
-$file = fopen("ct.txt", "a+");
-$file_ct_log = $file;
+if ($ct_path) {
+    $dir = dirname($ct_path);
+    if (!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    }
+    $file = fopen($ct_path, "a+");
+    $file_ct_log = $file;
+}
+if ($ct_log_path) {
+    $dir = dirname($ct_log_path);
+    if (!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    }
+    $file_ct_log = fopen($ct_log_path, "a+");
+}
 
-// comment out this if you DO NOT want to separate to tow ct files.
-// $file_ct_log = fopen("../runtime/logs/ct_log.txt", "a+");
 $ct_buffer = [];
 
 $path_my = __DIR__ . "/common.my.php";
@@ -92,11 +104,11 @@ function get_prefix() {
 }
 
 function CT($content) {
-    Global $CT_off;
-    if($CT_off)
+    Global $CT_off, $file;
+    if($CT_off || !$file)
         return;
 
-    Global $file, $last_time, $first_time, $is_first, $ct_log_buffer, $ct_buffer, $request_num, $CT_format;
+    Global $last_time, $first_time, $is_first, $ct_log_buffer, $ct_buffer, $request_num, $CT_format;
     if ($CT_format == "raw") {
         $all_out = $content . "\n";
     } else {
@@ -143,6 +155,9 @@ function CT($content) {
 function CT_flush($turn_off_buffer)
 {
     global $file, $ct_buffer, $ct_log_buffer;
+    if (!$file) {
+        return;
+    }
     fwrite($file, join("", $ct_buffer));
     $ct_buffer = [];
     $ct_log_buffer = !$turn_off_buffer;
@@ -150,8 +165,12 @@ function CT_flush($turn_off_buffer)
 
 function CT_log($content = "", $path = NULL) {
     Global $file_ct_log;
+    if (!$file_ct_log && !$path) {
+        return;
+    }
     $content = get_prefix().print_r($content, true)."\n";
     if($path) {
+        file_create_path($path);
         $file = fopen($path, "a+");
         if($file) {
             fwrite($file, $content);
@@ -283,6 +302,15 @@ function http_get_core($url, &$status = null)
     }
 }
 
+// 递归为path创建必要的路径
+function file_create_path($path)
+{
+    $dir = dirname($path);
+    if ($dir && !file_exists($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 // $save_path 需要gbk编码
 function file_save($content, $save_path, $append)
 {
@@ -330,4 +358,195 @@ function curl_do($url, $close_after_use = true)
     return array("handle" => $ch, "response" => $response, "err" => $err);
 }
 
+/**
+ * url拼接，没有处理user，pass两个components
+ * 详细定义参见单元测试
+ * @param $url
+ * @param $base
+ * @return string
+ */
+function get_absolute_url($url, $base)
+{
+    // 两种情况直接返回$url:
+    if (!$base)
+        return $url;
+    $url_host = parse_url($url, PHP_URL_HOST);
+    if ($url_host) {
+        return $url;
+    }
+
+    $base_parsed = parse_url($base);
+    $base_scheme = get_safe($base_parsed, "scheme", "");
+    $base_host = get_safe($base_parsed, "host", "");
+    $base_port = isset($base_parsed["port"]) ? ":" . $base_parsed["port"] : "";
+    $base_path = get_safe($base_parsed, "path");
+
+    if ($base_host) {
+        $base_calc = $base_scheme . "://" . $base_host . $base_port;
+        if (startsWith($url, "/")) {
+            return $base_calc . $url;
+        } else if ($base_path) {
+            $pos = strrpos($base_path, "/");
+            if ($pos !== false) {
+                $dir = substr($base_path, 0, $pos + 1); // with last "/"
+                return $base_calc . $dir . $url;
+            } else {
+                return $base_calc . "/" . $url;
+            }
+        } else {
+            return $base_calc . "/" . $url;
+        }
+    } else {
+        if (startsWith($url, "/")) {
+            return $url;
+        } else if ($base_path) {
+            $pos = strrpos($base_path, "/");
+            if ($pos !== false) {
+                $dir = substr($base_path, 0, $pos + 1); // with last "/"
+                return $dir . $url;
+            } else {
+                return $url;
+            }
+        } else {
+            return $url;
+        }
+    }
+}
+
+function get_absolute_url_tests()
+{
+    // empty tests
+    $tests[] = [null, null, null];
+    $tests[] = ["", null, ""];
+    $tests[] = [null, "", null];
+    $tests[] = ["", "", ""];
+
+    $tests[] = [null, "/", "/"];
+    $tests[] = [null, "/a", "/"];
+    $tests[] = [null, "/a/", "/a/"];
+
+    $tests[] = ["a.html", "b", "a.html"];
+    $tests[] = ["a.html", "", "a.html"];
+    $tests[] = ["a.html", "/", "/a.html"];
+    $tests[] = ["a/b/c/a.html", "http://1.1.1.1", "http://1.1.1.1/a/b/c/a.html"];
+    $tests[] = ["a.html", "http://1.1.1.1:83", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["a.html", "http://1.1.1.1:83/", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["a.html", "http://1.1.1.1:83/a/b", "http://1.1.1.1:83/a/a.html"];
+    $tests[] = ["a.html", "http://1.1.1.1:83/?", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["a.html", "http://1.1.1.1:83?", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["a.html", "http://1.1.1.1:83?a=b", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["a.html", "https://1.1.1.1:83?a=b#1", "https://1.1.1.1:83/a.html"];
+    $tests[] = ["a.html", "www.baidu.com?a=b#1", "a.html"]; // www被认为是path
+
+    // starts with "/"
+    $tests[] = ["/a.html", "b", "/a.html"];
+    $tests[] = ["/a.html", "", "/a.html"];
+    $tests[] = ["/a.html", "/", "/a.html"];
+    $tests[] = ["/a/b/c/a.html", "http://1.1.1.1", "http://1.1.1.1/a/b/c/a.html"];
+    $tests[] = ["/a.html", "http://1.1.1.1:83", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["/a.html", "http://1.1.1.1:83/", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["/a.html", "http://1.1.1.1:83/a/b", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["/a.html", "http://1.1.1.1:83/?", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["/a.html", "http://1.1.1.1:83?", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["/a.html", "http://1.1.1.1:83?a=b", "http://1.1.1.1:83/a.html"];
+    $tests[] = ["/a.html", "https://1.1.1.1:83?a=b#1", "https://1.1.1.1:83/a.html"];
+    $tests[] = ["/a.html", "www.baidu.com?a=b#1", "/a.html"]; // www被认为是path
+
+    $r = true;
+    foreach ($tests as $test) {
+        $abs = get_absolute_url($test[0], $test[1]);
+        echo $abs . "\n";
+        if ($abs !== $test[2]) {
+            $r = false;
+            break;
+        }
+    }
+    echo "pass: " . $r . "\n";
+
+    // return self tests
+    $tests_self = [];
+    $tests_self[] = ["http://test.com/a.html", "b", ""];
+    $tests_self[] = ["http://test.com/a.html", "", ""];
+    $tests_self[] = ["http://test.com/a.html", "/", ""];
+    $tests_self[] = ["http://test.com/a/b/c/a.html", "http://1.1.1.1", ""];
+    $tests_self[] = ["http://test.com/a.html", "http://1.1.1.1:83", ""];
+    $tests_self[] = ["http://test.com/a.html", "http://1.1.1.1:83/", ""];
+    $tests_self[] = ["http://test.com/a.html", "http://1.1.1.1:83/a/b", ""];
+    $tests_self[] = ["http://test.com/a.html", "http://1.1.1.1:83/?", ""];
+    $tests_self[] = ["http://test.com/a.html", "http://1.1.1.1:83?", ""];
+    $tests_self[] = ["http://test.com/a.html", "http://1.1.1.1:83?a=b", ""];
+    $tests_self[] = ["http://test.com/a.html", "https://1.1.1.1:83?a=b#1", ""];
+    $tests_self[] = ["http://test.com/a.html", "www.baidu.com?a=b#1", ""]; // www被认为是path
+
+    echo "-------------------return self test--------:\n";
+    $r = true;
+    foreach ($tests_self as $test) {
+        $abs = get_absolute_url($test[0], $test[1]);
+        echo $abs . "\n";
+        if ($abs !== $test[0]) {
+            $r = false;
+            break;
+        }
+    }
+    echo "pass: " . $r . "\n";
+}
+
+// 尝试gbk、utf-8两种编码；优先尝试传入编码
+function is_file_ex($path)
+{
+    if (is_file($path)) {
+        return true;
+    }
+    $enc = mb_detect_encoding($path, "gb2312", true);
+    if ($enc === 'EUC-CN') {
+        $path2 = iconv("gbk", "utf-8", $path);
+    } else {
+        $path2 = iconv("utf-8", "gbk", $path);
+    }
+    return is_file($path2);
+}
+
+// 尝试gbk、utf-8两种编码；优先尝试传入编码
+function file_get_contents_ex($path)
+{
+    if (is_file($path)) {
+        return file_get_contents($path);
+    }
+    $enc = mb_detect_encoding($path, "gb2312", true);
+    if ($enc === 'EUC-CN') {
+        $path2 = iconv("gbk", "utf-8", $path);
+    } else {
+        $path2 = iconv("utf-8", "gbk", $path);
+    }
+    if (is_file($path2)) {
+        return file_get_contents($path2);
+    }
+    return false;
+}
+
+// 创建文件lock，如果路径不存在则创建之
+function create_file_lock($path, &$output = null)
+{
+    file_create_path($path);
+    $f = null;
+    // 不使用"@"，这样忽略文件存在的报错，其他异常返回（如权限问题）
+    try {
+        $f = fopen($path, "x");
+    } catch (Exception $e) {
+        $msg = $e->getMessage();
+        if (strpos($msg, "File exists") === false && strpos($msg, "文件已存在") === false) {
+            $output = $e->getMessage() . "\n" . $e->getTraceAsString();
+        }
+    }
+    return $f;
+}
+
+// 关闭、释放文件lock
+function release_file_lock($f, $path)
+{
+    if ($f) {
+        fclose($f);
+        unlink($path);
+    }
+}
 ?>
