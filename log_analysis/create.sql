5869a4a1534eb2e034e2ef56119e2b4a0e2595a2
yuebin.chen <cyb@piaojuke.com>
2018-09-08 14:07:59 +0800

    删除无用的sql

--- log_analysis/create.sql
+++ log_analysis/create.sql
@@ -1,9 +1,3 @@
-create database `log_data` default character set utf8 collate utf8_general_ci;
-#drop database `log_data`;
-use `log_data`;
-#mysqldump -uroot log_data >D:\log_data.bak -p  #备份
-#mysql -uroot log_data < D:\log_data.bak -p  #还原
-
 -- 日志查看行数存储
 DROP TABLE IF EXISTS `mynote_indexfile`;
 
